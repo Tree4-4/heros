be21d83b23ba102f65d1ebfad53f4810caa97e32
Tree4-4 <trey65ts@gmail.com>
2025-02-16 17:41:55 -0600

    java

---
+++ js/app.js
@@ -0,0 +1,92 @@
+const heros = [
+    {
+        id: 77982,
+        hero: 3-dman,
+        power: create-anything,
+        age: 40,
+        hair: black,
+        height: tall,
+        created: 1977
+    },
+    {
+        id: 69050,
+        hero: ironman,
+        power: very-smart,
+        age: 53,
+        hair: black,
+        height: average,
+        created: 1963
+    }, 
+    {
+        id: 68595,
+        hero: black-window,
+        power: enhance-human,
+        age: 32,
+        hair: ginger,
+        height: average,
+        created: 1964
+    },
+    {
+        id: 75968,
+        hero: green-latern,
+        power: imagination,
+        age: 86,
+        hair: brown,
+        height: tall,
+        created: 1940
+    },
+    {
+        id: 10102,
+        hero: spawn,
+        power: demonic-attributes,
+        age: 56/immortal,
+        hair: none,
+        height: tall,
+        created: 1978
+    },
+    {
+        id: 69044,
+        hero: beast-boy,
+        power: shape-shifter,
+        age: 18,
+        hair: green,
+        height: short,
+        created: 1965
+    },
+    {
+        id: 79430,
+        hero: cybrog,
+        power: andriod,
+        age: 22,
+        hair: black,
+        height: tall,
+        created: 1960
+    },
+    {
+        id: 10102,
+        hero: spider-man,
+        power: spidey-powers,
+        age: 19,
+        hair: brown,
+        height: average,
+        created: 1962
+    },
+    {
+        id: 65770,
+        hero: invicible,
+        power: innovable,
+        age: 19,
+        hair: black,
+        height: tall,
+        created: 2003
+    },
+    {
+        id: 58930,
+        hero: rex-spold,
+        power: expolsive,
+        age: 20,
+        hair: brown,
+        height: average,
+        create: 2003
+    }
+]
